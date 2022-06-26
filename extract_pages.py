71ba6874bb0a655aaa83e01425b330a055275ae7
Feihong Hsu <feihong.hsu@gmail.com>
2022-06-26 10:46:22 -0500

    When extracting images, ignore files which aren't images

--- extract_pages.py
+++ extract_pages.py
@@ -14,14 +14,24 @@ def main():
   prefix = input_file.stem
 
   with zipfile.ZipFile(input_file) as zf:
-    paths = zf.namelist()
+    image_paths = list(get_image_paths(zf))
+
     for i, page in enumerate(pages, 1):
-      image_path = paths[page - 1]
+      image_path = image_paths[page - 1]
       image_ext = image_path.rsplit('.')[-1]
       image_data = zf.read(image_path)
       output_file = Path(f'{prefix} {page:03}.{image_ext}')
       output_file.write_bytes(image_data)
       print(f'Wrote {i}. {output_file}')
+
+def get_image_paths(zf: zipfile.ZipFile):
+  """
+  Return the paths of all jpg/png files inside the given zipfile
+  """
+  for path in zf.namelist():
+    ext = path.rsplit('.')[-1].lower()
+    if ext in ['jpg', 'png']:
+      yield path
 
 if __name__ == '__main__':
   main()
