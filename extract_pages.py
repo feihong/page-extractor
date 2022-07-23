54567ae34909ae519d5a1903d70f4f1e0c24e71c
Feihong Hsu <feihong.hsu@gmail.com>
2022-07-23 09:28:31 -0500

    Reduce time separation between extracted images from 1 minute to 10 seconds

--- extract_pages.py
+++ extract_pages.py
@@ -6,21 +6,17 @@ from pathlib import Path
 import time
 import os
 
-from settings import books_dir
 import util
-import convert
 
 ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def main():
   extraction = util.get_extraction()
-  if extraction.is_kepub():
-    input_file = convert.convert(extraction.path)
 
   prefix = extraction.path.stem
   curr_time = time.time() - ONE_DAY_IN_SECONDS
 
-  with zipfile.ZipFile(input_file) as zf:
+  with zipfile.ZipFile(extraction.path) as zf:
     image_paths = list(get_image_paths(zf))
 
     for i, page in enumerate(extraction.pages, 1):
@@ -29,9 +25,12 @@ def main():
       image_data = zf.read(image_path)
       output_file = Path(f'{prefix} {page:03}.{image_ext}')
       output_file.write_bytes(image_data)
+
+      # Change modified time so images appear in the expected order when
+      # uploaded to Google Photos
       os.utime(output_file, times=(curr_time, curr_time))
       print(f'Wrote {i}. {output_file}')
-      curr_time += 60
+      curr_time += 10
 
 def get_image_paths(zf: zipfile.ZipFile):
   """
