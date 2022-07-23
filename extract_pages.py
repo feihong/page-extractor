e95da41bb0f2aa93e16183a741efa809d00b688f
Feihong Hsu <feihong.hsu@gmail.com>
2022-07-22 22:44:44 -0500

    Change modification times of extracted images so they're one minute apart

--- extract_pages.py
+++ extract_pages.py
@@ -1,10 +1,16 @@
-
+"""
+Extract page images from comic files according to extractions.txt
+"""
 import zipfile
 from pathlib import Path
+import time
+import os
 
 from settings import books_dir
 import util
 import convert
+
+ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def main():
   extraction = util.get_extraction()
@@ -12,6 +18,7 @@ def main():
     input_file = convert.convert(extraction.path)
 
   prefix = extraction.path.stem
+  curr_time = time.time() - ONE_DAY_IN_SECONDS
 
   with zipfile.ZipFile(input_file) as zf:
     image_paths = list(get_image_paths(zf))
@@ -22,7 +29,9 @@ def main():
       image_data = zf.read(image_path)
       output_file = Path(f'{prefix} {page:03}.{image_ext}')
       output_file.write_bytes(image_data)
+      os.utime(output_file, times=(curr_time, curr_time))
       print(f'Wrote {i}. {output_file}')
+      curr_time += 60
 
 def get_image_paths(zf: zipfile.ZipFile):
   """
